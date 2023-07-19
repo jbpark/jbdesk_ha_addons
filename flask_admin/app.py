2002695dfe452eceb5461d3db97b1188eb013812
jaebok.park <jaebok.park@mytrum.com>
2023-07-19 10:03:07 +0900

    ..

--- flask_admin/app.py
+++ flask_admin/app.py
@@ -1,10 +1,15 @@
 from flask import Flask
 from flask_admin import Admin
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 # set optional bootswatch theme
 app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:1234@localhost:3306/testdb"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
 
 admin = Admin(app, name='microblog', template_mode='bootstrap3')
 # Add administrative views here
